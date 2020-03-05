ae46d6991bc21a7be45e284288889ee68d7b608b
name <fanxiaojuan@zork.com.cn>
2020-03-05 10:31:40 +0800

    Flutter Container组件、Text组件详解

--- lib/main_contaier_text.dart
+++ lib/main_contaier_text.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Demo~~~~~~3'),
+          title: Text('Flutter Demo~~~~~~2'),
         ),
         body: HomeContent(),
       ),
--- lib/main_materialapp_scaffold.dart
+++ lib/main_materialapp_scaffold.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-
-// MaterialApp组件、Scaffold组件
-
-
-// void main() {
-//   //new关键字可以省略
-//   runApp(MyApp());
-// }
-
+/* 
+ 件用MaterialApp 和 Scaffold两个组件装饰App    
+   01 MaterialApp    
+   02 Scaffold
+ */
 //也可以简写
 void main()=>runApp(MyApp());
 
@@ -15,7 +11,11 @@ void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+     // 使用 Material 组件
+    // Flutter提供了许多widgets，可帮助您构建遵循Material Design的应用程序。Material应用程序以MaterialApp widget开始， 该widget在应用程序的根部创建了一些有用的widget，其中包括一个Navigator， 它管理由字符串标识的Widget栈（即页面路由栈）。Navigator可以让您的应用程序在页面之间的平滑的过渡。 是否使用MaterialApp完全是可选的，但是使用它是一个很好的做法。
+    
     return MaterialApp(
+      //Scaffold是Material中主要的布局组件.
       home: Scaffold(
         appBar: AppBar(
           title: Text(
